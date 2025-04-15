5d18c637e9c8f9b77979788da3ad49076b0bb3e5
Novitnit <nachanoch.pan@gmail.com>
2025-04-15 20:45:27 +0700

    add bar

---
+++ src/content/bar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from '../css/content/bar.module.css'
+export default function Bar() {
+    return (
+      <div className={style.bar}>
+        <p>bar</p>
+      </div>
+    )
+}
